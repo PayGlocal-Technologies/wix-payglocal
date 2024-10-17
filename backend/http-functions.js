bf241169746103933e635d62966ecee8c9868f6f
Rohan <rohan.aggarwal@payglocal.in>
2024-10-17 17:17:58 +0530

    Update http-functions.js

--- backend/http-functions.js
+++ backend/http-functions.js
@@ -2,20 +2,22 @@ import wixPaymentProviderBackend from 'wix-payment-provider-backend';
 import { ok, badRequest } from 'wix-http-functions';
 
 // This endpoint is used to update the status of a checkout session transaction.
-export async function post_updateTransaction(request) {
+export async function get_updateTransaction(request) {
     const response = {
         'headers': {
             'Content-Type': 'application/json'
         }
     };
-    let status = request.query['status'];
-    let txn_no = request.query['gid'];
+    let token = request.query['xglToken'];
+    const res = await validateJweSignature(token, PUBLIC_KEY);
+    
+    let txn_no = res.data.gid;
+    let status = res.data.status;
+    let wixTransactionId = res.data.wixTransactionId;
 
     if (status === 'SENT_FOR_CAPTURE') {
         try {
-            let wixTransactionId = request.query['wixTransactionId'];
             await submitTransactionUpdate(wixTransactionId, txn_no);
-
             response.body = {};
             return ok(response);
         } catch (err) {
@@ -36,13 +38,19 @@ export async function post_updateRefund(request) {
             'Content-Type': 'application/json'
         }
     };
+
+    let token = request.query['xglToken'];
+    console.log("token is ", token)
+    const res = await validateJweSignature(token, PUBLIC_KEY);
+
+    const status = res.data.status;
+    const amount = res.data.amount;
+    const wixRefundId = res.data.refundId;
+    const wixTransactionId = res.data.wixTransactionId;
+    const pluginRefundId = res.data.pluginTransactionId;
+    
     if (request.query.status === 'SENT_FOR_REFUND') {
         try {
-            const amount = request.query['amount'];
-            const wixRefundId = request.query['refundId'];
-            const wixTransactionId = request.query['wixTransactionId'];
-            const pluginRefundId = request.query['pluginTransactionId'];
-
             await submitRefundUpdate(wixTransactionId, pluginRefundId, wixRefundId, amount)
             response.body = {};
             return ok(response);
